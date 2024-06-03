937a811c73f10e68a7990e6482d7a9582e20f760
linyh <linyh_xmcu_edu@163.com>
2024-06-03 23:05:47 +0800

    第四次提交，完成志愿者基本功能

--- src/main/java/com/edu/smartpersionsys/pojo/Volunteer.java
+++ src/main/java/com/edu/smartpersionsys/pojo/Volunteer.java
@@ -2,20 +2,28 @@ package com.edu.smartpersionsys.pojo;
 
 import lombok.Data;
 
+
+//这个志愿者类包括了user中的字段，
 @Data
 public class Volunteer {
     private Integer vId; //志愿者ID
+    private Integer userId; //用户id
     private Integer totalTime;//志愿总时常
+    private Integer taskTotal; //接收任务数量
     private String vName;//志愿者姓名
     private String vNamePhoto;//志愿者头像
+    private String vPhone; // 联系电话
 
     public Volunteer() {
     }
 
-    public Volunteer(Integer vId, Integer totalTime, String vName, String vNamePhoto) {
+    public Volunteer(Integer vId, Integer userId, Integer totalTime, Integer taskTotal, String vName, String vNamePhoto, String vPhone) {
         this.vId = vId;
+        this.userId = userId;
         this.totalTime = totalTime;
+        this.taskTotal = taskTotal;
         this.vName = vName;
         this.vNamePhoto = vNamePhoto;
+        this.vPhone = vPhone;
     }
 }
--- src/main/java/com/edu/smartpersionsys/utils/OlderIncrease.java
+++ src/main/java/com/edu/smartpersionsys/utils/OlderIncrease.java
@@ -1,5 +1,12 @@
 package com.edu.smartpersionsys.utils;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class OlderIncrease {
     //增长率
     private  Integer conversionRate;
@@ -10,40 +17,4 @@ public class OlderIncrease {
     //养老院最多入住人数
     private final Integer finalNumber = 500;
 
-    public OlderIncrease(Integer conversionRate, Integer progressPercentage, Integer olderNumber) {
-        this.conversionRate = conversionRate;
-        this.progressPercentage = progressPercentage;
-        this.olderNumber = olderNumber;
-    }
-
-    public OlderIncrease() {
-    }
-
-    public Integer getFinalNumber() {
-        return finalNumber;
-    }
-
-    public Integer getConversionRate() {
-        return conversionRate;
-    }
-
-    public void setConversionRate(Integer conversionRate) {
-        this.conversionRate = conversionRate;
-    }
-
-    public Integer getProgressPercentage() {
-        return progressPercentage;
-    }
-
-    public void setProgressPercentage(Integer progressPercentage) {
-        this.progressPercentage = progressPercentage;
-    }
-
-    public Integer getOlderNumber() {
-        return olderNumber;
-    }
-
-    public void setOlderNumber(Integer olderNumber) {
-        this.olderNumber = olderNumber;
-    }
 }
